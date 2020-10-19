0cb4bb5bf1fb27a778817407ebbac7cea81a6224
Claudio Cenghialta <62311968+claudiocenghialta@users.noreply.github.com>
2020-10-19 13:04:26 +0200

    migration tabella user+avatar+relazione1a1+seeders

--- database/migrations/2020_10_19_095338_create_avatars_table.php
+++ database/migrations/2020_10_19_095338_create_avatars_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Avatar;
 
 class CreateAvatarsTable extends Migration
 {
@@ -15,7 +16,16 @@ class CreateAvatarsTable extends Migration
     {
         Schema::create('avatars', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('telefono',25);
+            $table->text('avatar');
+            // $table->timestamps();
+
+            //creiamo il campo per la chiave esterna
+            $table->unsignedBigInteger('user_id');
+            //facciamo la relazione con la tabella users
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+                
         });
     }
 
